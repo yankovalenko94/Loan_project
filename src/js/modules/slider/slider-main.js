c13120a219e85b9158471bcff2273cc234d6d7da
Ivan <ivan-kerch@mail.ru>
2020-02-04 16:04:31 +0300

    the main slider module is finished

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -1,9 +1,8 @@
-export default class Slider {
-    constructor(page, btns){
-        this.page = document.querySelector(page);
-        this.slides = this.page.children;
-        this.btns = document.querySelectorAll(btns);
-        this.slideIndex = 1;
+import Slider from './slider';
+
+export default class MainSlider extends Slider {
+    constructor(page, btns) {
+        super(page, btns);
     }
 
     showSlides(n) {
